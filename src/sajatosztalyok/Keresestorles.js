c92a0ce0b7f5fa3a3061044459c964ab0d3c3b72
adamfaragoo <adam.farago004@gmail.com>
2022-01-25 09:50:23 +0100

    a

--- src/sajatosztalyok/Keresestorles.js
+++ src/sajatosztalyok/Keresestorles.js
@@ -8,103 +8,48 @@ export default class FetchExample extends React.Component {
 
   constructor(props){
     super(props);
-    this.state ={ isLoading: true}
+    this.state ={ 
+        isLoading: true,
+        dataSource:[],
+        szoveg:''
+    }
   }
 
   componentDidMount(){
     document.body.style.backgroundColor = "#262626" 
-    return fetch('http://'+ipcim+'/sorozat')
-      .then((response) => response.json())
-      .then((responseJson) => {
-
-        this.setState({
-          isLoading: false,
-          dataSource: responseJson,
-        }, function(){
-
-        });
-
-      })
-      .catch((error) =>{
-        console.error(error);
-      });
   }
 
-  torles=(szam)=>{
-    //alert(szam)
-    var bemenet={
-      bevitel1:szam
-    }
-
-  fetch('http://'+ipcim+'/sorozattorles', {
-      method: "POST",
-      body: JSON.stringify(bemenet),
-      headers: {"Content-type": "application/json; charset=UTF-8"}
-    }
   
-  )
-  .then(x => x.text())
-  .then(y => alert(y));
-
-  fetch('http://'+ipcim+'/sorozat')
-      .then((response) => response.json())
-      .then((responseJson) => {
-
-        this.setState({
-          isLoading: false,
-          dataSource: responseJson,
-        }, function(){
-
-        });
-
-      })
-      .catch((error) =>{
-        console.error(error);
-      });
-
-      
-  }
-  kereses=async () =>{
-    //alert(this.state.cim)
+  kereses= () =>{
     let bemenet ={
-      bevitel1:this.state.cim,
-
-
+      bevitel1:this.state.szoveg,
     }
-    fetch('http://'+ipcim+'/kereses', {
-     method: "POST",
-     body: JSON.stringify(bemenet),
-     headers: {"Content-type": "application/json; charset=UTF-8"}
-   }
-   )
-     .then((response) => response.json())
-     .then((responseJson) => {
-       this.setState({
-         isLoading: false,
-         dataSource: responseJson,
-       }, function(){
-        //alert(JSON.stringify(this.state.dataSource))
-
-       });
-
-     })
-     .catch((error) =>{
-       console.error(error);
-     });
-
- 
-   }
-
+    fetch('http://'+ipcim+'/osszessorozatkomment', {
+        method: "POST",
+        body: JSON.stringify(bemenet),
+        headers: {"Content-type": "application/json; charset=UTF-8"}
+      }
+      )
+        .then((response) => response.json())
+        .then((responseJson) => {
+   
+          this.setState({
+            isLoading: false,
+            dataSource: responseJson,
+          }, function(){
+   
+          });
+   
+        })
+        .catch((error) =>{
+          console.error(error);
+        });
+   
+    
+      }
+   
 
   render(){
-
-    if(this.state.isLoading){
-      return(
-        <View style={{flex: 1, padding: 20}}>
-          <ActivityIndicator/>
-        </View>
-      )
-    }
 
     return(
       <View style={{flex: 1, paddingTop:20}}>
@@ -114,18 +59,27 @@ export default class FetchExample extends React.Component {
         placeholderTextColor="black"
         style={{height: 45,backgroundColor:"#DCDCDC", borderRadius:10, padding:10, width:240,margin:20,marginRight:10, textAlign:"center", }}
         placeholder="Keresés"
-        onChangeText={(cim) => this.setState({cim})}
-        value={this.state.cim}
+        onChangeText={(szoveg) => this.setState({szoveg})}
+        value={this.state.szoveg}
         />
         <TouchableOpacity 
-          onPress={async ()=>this.kereses()}>
+          onPress={ ()=>this.kereses()}>
           <View style={{width:85,height:50,backgroundColor:"#2596be", borderRadius:10,height:45, alignItems:'center', justifyContent:'center'}}>
         
             <Text>Keresés</Text>
           </View>
         </TouchableOpacity>
       </View>
-      
+
+      <FlatList
+        data={this.state.dataSource}
+        keyExtractor={({komment_id}) => komment_id}
+        renderItem={({item}) => 
+        <View>
+            <Text>{item.komment_szoveg}</Text>
+        </View>
+         }
+        />
       </View>
       
 
