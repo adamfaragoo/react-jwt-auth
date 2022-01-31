631370f95cb644c637883af9ac2287d6dd710eb3
adamfaragoo <adam.farago004@gmail.com>
2022-01-31 11:09:25 +0100

    a

--- src/sajatosztalyok/Keresestorles.js
+++ src/sajatosztalyok/Keresestorles.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import {StyleSheet, FlatList, ActivityIndicator, Text, View, Image , TouchableOpacity, TextInput } from 'react-native-web';
-
+import {StyleSheet, FlatList, ActivityIndicator, Text, View, Image , TouchableOpacity, TextInput, Dimensions } from 'react-native-web';
+var height = Dimensions.get("window").height;
+var width = Dimensions.get("window").width;
 
 const ipcim="localhost:8080";
 
@@ -11,7 +12,9 @@ export default class FetchExample extends React.Component {
     this.state ={ 
         isLoading: true,
         dataSource:[],
-        szoveg:''
+        dataSource2:[],
+        szoveg:'',
+        szoveg2:''
     }
   }
 
@@ -44,17 +47,47 @@ export default class FetchExample extends React.Component {
         .catch((error) =>{
           console.error(error);
         });
-   
     
       }
-   
+      
+    kereses2= () =>{
+      let bemenet2 ={
+        bevitel2:this.state.szoveg2,
+      }
+      fetch('http://'+ipcim+'/osszesfilmkomment', {
+          method: "POST",
+          body: JSON.stringify(bemenet2),
+          headers: {"Content-type": "application/json; charset=UTF-8"}
+        }
+        )
+          .then((response) => response.json())
+          .then((responseJson) => {
+     
+            this.setState({
+              isLoading: false,
+              dataSource2: responseJson,
+            }, function(){
+     
+            });
+     
+          })
+          .catch((error) =>{
+            console.error(error);
+          });
+     
+      
+        }
+
 
   render(){
 
     return(
+    <View style={{alignItems:'center'}}> 
+      <View style={{alignItems:'center', backgroundColor:"", borderRadius:20, width:width*0.3, justifyContent:'center'}}>
+
       <View style={{flex: 1, paddingTop:20}}>
-       
-       <View style={{flexDirection:'column', alignItems:'center', justifyContent:'center'}}>
+        <Text style={{textAlign:'center', color:"white", fontSize:25}}>Sorozat kommentek törlése</Text>
+       <View style={{flexDirection:'column', alignItems:'center', justifyContent:'center', flexDirection:"row"}}>
         <TextInput
         placeholderTextColor="black"
         style={{height: 45,backgroundColor:"#DCDCDC", borderRadius:10, padding:10, width:240,margin:20,marginRight:10, textAlign:"center", }}
@@ -70,7 +103,7 @@ export default class FetchExample extends React.Component {
           </View>
         </TouchableOpacity>
       </View>
-<View style={{alignItems:'center'}}>
+      
       <FlatList
         data={this.state.dataSource}
         keyExtractor={({komment_id}) => komment_id}
@@ -83,8 +116,43 @@ export default class FetchExample extends React.Component {
          }
         />
       </View>
+      <View style={{flex: 1, paddingTop:20}}>
+        <Text style={{textAlign:'center', color:"white", fontSize:25}}>Film kommentek törlése</Text>
+       <View style={{flexDirection:'column', alignItems:'center', justifyContent:'center', flexDirection:"row"}}>
+        <TextInput
+        placeholderTextColor="black"
+        style={{height: 45,backgroundColor:"#DCDCDC", borderRadius:10, padding:10, width:240,margin:20,marginRight:10, textAlign:"center", }}
+        placeholder="Keresés"
+        onChangeText={(szoveg2) => this.setState2({szoveg2})}
+        value={this.state.szoveg2}
+        />
+        <TouchableOpacity 
+          onPress={ ()=>this.kereses2()}>
+          <View style={{width:85,height:50,backgroundColor:"#2596be", borderRadius:10,height:45, alignItems:'center', justifyContent:'center'}}>
+        
+            <Text>Keresés</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
+      
+      <FlatList
+        data={this.state.dataSource2}
+        keyExtractor={({film_komment_id}) => film_komment_id}
+        renderItem={({item}) => 
+        <View style={{justifyContent:'center', borderWidth:1,width:150,borderColor:"transparent",borderRadius:10,padding:8,backgroundColor:"lightgrey",margin:7,marginLeft:15}}>
+            <Text style={{color:"black",fontWeight:"bold",fontSize:15}}>{item.film_komment_nev}</Text>
+            <Text style={{fontSize:17}}>{item.film_komment_szoveg}</Text>
+
+        </View>
+         }
+        />
+      </View>
     </View>  
 
+  </View>  
+
     );
+    
   }
+  
 }
